b56b5d4ce4e116e11ea1d1640bb07e7f9e4d209b
naingwin-aung <naingwinaung1710@gmail.com>
2025-02-11 11:40:35 +0700

    [Fix] scroll top

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,6 +1,6 @@
 <script setup>
 import SidebarItem from "@/Components/sidebar/SidebarItem.vue";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { Link } from "@inertiajs/vue3";
 
 const userMenus = ref([
@@ -9,35 +9,51 @@ const userMenus = ref([
         label: "Dashboard",
         url: "/dashboard",
     },
-    {
-        icon: "fa-solid fa-person-circle-exclamation",
-        label: "Roles",
-        url: "/roles",
-    },
-    {
-        icon: "fa-solid fa-person-circle-exclamation",
-        label: "Users",
-        url: "/users",
-    },
     // {
-    //     icon: "fa-solid fa-layer-group",
-    //     label: "Settings",
-    //     url: "#",
-    //     // permission: ["access_main_title", "access_main_title_text"],
-    //     children: [
-    //         {
-    //             icon: "fa-solid fa-house",
-    //             label: "Roles",
-    //             url: "/roles",
-    //         },
-    //         {
-    //             icon: "fa-solid fa-house-chimney",
-    //             label: "Users",
-    //             url: "/users",
-    //         },
-    //     ],
+    //     icon: "fa-solid fa-person-circle-exclamation",
+    //     label: "Roles",
+    //     url: "/roles",
     // },
+    // {
+    //     icon: "fa-solid fa-user",
+    //     label: "Users",
+    //     url: "/users",
+    // },
+    {
+        icon: "fa-solid fa-layer-group",
+        label: "Settings",
+        url: "#",
+        // permission: ["access_main_title", "access_main_title_text"],
+        children: [
+            {
+                icon: "fa-solid fa-house",
+                label: "Roles",
+                url: "/roles",
+            },
+            {
+                icon: "fa-solid fa-house-chimney",
+                label: "Users",
+                url: "/users",
+            },
+        ],
+    },
 ]);
+
+const sidebarScroll = ref(null);
+
+// Save scroll position before navigation
+const saveScrollPosition = () => {
+    const scrollPosition = sidebarScroll.value?.scrollTop || 0;
+    localStorage.setItem('sidebarScrollPosition', scrollPosition.toString());
+};
+
+// Restore scroll position after navigation
+onMounted(() => {
+    const savedPosition = localStorage.getItem('sidebarScrollPosition');
+    if (savedPosition && sidebarScroll.value) {
+        sidebarScroll.value.scrollTop = parseInt(savedPosition);
+    }
+});
 </script>
 
 <template>
@@ -53,7 +69,11 @@ const userMenus = ref([
             </div>
 
             <div class="flex flex-col justify-between h-[calc(100vh-4rem)]">
-                <ul class="flex flex-col gap-2.5 pt-4 pb-8 px-2 overflow-y-auto hidden-scrollbar">
+                <ul 
+                    ref="sidebarScroll"
+                    class="flex flex-col gap-2.5 pt-4 pb-8 px-2 overflow-y-auto hidden-scrollbar"
+                    @scroll="saveScrollPosition"
+                >
                     <template v-for="menu in userMenus">
                         <SidebarItem :item="menu" />
                     </template>
@@ -61,14 +81,15 @@ const userMenus = ref([
 
                 <div class="border-t border-[#e5e7eb]">
                     <ul class="flex flex-col gap-2.5 pt-4 pb-8 px-2">
-                        <Link :href="route('logout')" method="post" as="button">
-                            <li
-                                class="cursor-pointer hover:bg-black hover:text-white px-4 text-black py-2 rounded-lg text-left"
-                            >
+                        <Link 
+                            :href="route('logout')" 
+                            method="post" 
+                            as="button"
+                            @click="localStorage.removeItem('sidebarScrollPosition')"
+                        >
+                            <li class="cursor-pointer hover:bg-black hover:text-white px-4 text-black py-2 rounded-lg text-left">
                                 <span class="flex items-center gap-3">
-                                    <i
-                                        class="fa-solid fa-right-from-bracket"
-                                    ></i>
+                                    <i class="fa-solid fa-right-from-bracket"></i>
                                     Logout
                                 </span>
                             </li>
@@ -86,3 +107,14 @@ const userMenus = ref([
         </div>
     </div>
 </template>
+
+<style>
+.hidden-scrollbar {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+}
+
+.hidden-scrollbar::-webkit-scrollbar {
+    display: none;
+}
+</style>
